2ee0d6b147d50edd578912d66023c118d98d9d5a
Caroline Jin <cljin@mit.edu>
2021-01-16 14:41:51 -0500

    Added go home and start button functionality

--- client/src/components/modules/NewGame.js
+++ client/src/components/modules/NewGame.js
@@ -1,15 +1,46 @@
 import React, { Component } from "react";
-
+import { navigate } from "@reach/router";
 import "../../utilities.css";
 
+/**
+ * This is the NewGame component which gives the player the option to start
+ * game or go home
+ * 
+ * Proptypes
+ * @param {Hunt} hunt a hunt following the hunt model schema
+ * @param {()=>void} onStart function to call to update the parent state
+ */
 class NewGame extends Component {
     constructor(props){
         super(props);
     }
 
+    handleGoHome = () => {
+        navigate("/userhome");
+    }
+
+    handleStart = () => {
+        this.props.onStart && this.props.onStart();
+    }
+
     render(){
         return (
-            <div>This is the new game component</div>
+            <div>
+                <h2>{this.props.hunt.title}</h2>
+                <h4>{this.props.hunt.description}</h4>
+                <div>
+                    <button
+                        onClick = {this.handleGoHome}
+                    >
+                        {"<go home/>"}
+                    </button>
+                    <button
+                        onClick = {this.handleStart}
+                    >
+                        {"<start/>"}
+                    </button>
+                </div>
+            </div>
         );
     }
 }
--- client/src/components/pages/Game.js
+++ client/src/components/pages/Game.js
@@ -3,17 +3,40 @@ import NewGame from "../modules/NewGame.js";
 import PlayHunt from "../modules/PlayHunt.js";
 import "../../utilities.css";
 
+/**
+ * This is the game page that displays starting the game and
+ * playing the game
+ */
 class Game extends Component {
     constructor(props){
         super(props);
+
+        this.state = {
+            start: false,
+        }
     }
 
+    startGame = () => {
+        this.setState({
+            start: true,
+        });
+    };
+
     render(){
+        const data = {
+            hunt: {
+                title: "This is a hardcoded title",
+                description: "This is a hardcoded description"
+            }
+        };
+
+        let display = (this.state.start) ?  (<PlayHunt />) : (<NewGame hunt = {data.hunt} 
+                                                                       onStart = {this.startGame}
+                                                            />);
         return (
             <div>
-                <div>This is the game page</div>
-                <NewGame />
-                <PlayHunt />
+                <h3>This is the game page</h3>
+                {display}
             </div>
 
         );
