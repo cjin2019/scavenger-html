6b4e1571f5926f16c4771aef0f3549ff0d542a05
Caroline Jin <cljin@mit.edu>
2021-01-16 14:10:16 -0500

    Added basic code to game file

---
+++ client/src/components/pages/Game.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+import "../../utilities.css";
+
+class Game extends Component {
+    constructor(props){
+        super(props);
+    }
+}
+
+export default Game;
