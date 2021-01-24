a9506f16cc213f502681aaef565091fb4bea1114
Caroline Jin <cljin@mit.edu>
2021-01-24 10:57:40 -0500

    Made force user login nicer

--- client/src/components/pages/PageFunctions.js
+++ client/src/components/pages/PageFunctions.js
@@ -1,14 +1,15 @@
 import React from "react";
 import { navigate } from "@reach/router";
 import "./ButtonPage.css"
+import "./ForceUserLogin.css";
 
 /**
  * @param {string} userId the id of the user
  * @param {Component} display what to display if the user is logged in
  */
 const forceUserLogin = (userId, display) => {
-    let displayLogin = (<div>
-        <div>Go to login page first</div>
+    let displayLogin = (<div className = "ForceUserLogin-container">
+        <h1>Go to login page first</h1>
         <button 
             onClick = {() => navigate("/")}
             className = "ButtonPage-button"
